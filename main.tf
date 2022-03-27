7aedb753bf28ace6c50a6fac152fd49cdfbff426
sufiyaneng <71214149+sufiyaneng@users.noreply.github.com>
2022-03-27 11:29:42 +0530

    Update main.tf
    update

--- main.tf
+++ main.tf
@@ -38,8 +38,8 @@ resource "azurerm_windows_virtual_machine" "winvm0" {
   resource_group_name = azurerm_resource_group.azvm-rg.name
   location            = azurerm_resource_group.azvm-rg.location
   size                = "Standard_F2"
-  admin_username      = "adminuser"
-  admin_password      = "P@$$w0rd1234!"
+  admin_username      = "azure"
+  admin_password      = "Welcome12345"
   network_interface_ids = [
     azurerm_network_interface.aznetinterface.id,
   ]
